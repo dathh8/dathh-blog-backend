c122ee6a2d40122eb811d75bd6ab33f7072b5880
dathh <dathh@smartosc.com>
2025-05-12 18:40:21 +0700

    auth global

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,30 @@ import { UsersModule } from '@/modules/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/passport/jwt-auth.guard';
 
 @Module({
   imports: [
     UsersModule,
-    ConfigModule.forRoot( { isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
-  }),
-  inject: [ConfigService],
-}),
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule
 
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,12 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './passport/local-auth.guard';
 import { JwtAuthGuard } from './passport/jwt-auth.guard';
+import { Public } from '@/decorator/decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("login")
+  @Public()
   @UseGuards(LocalAuthGuard)
   handleLogin(@Request() request) {
     return this.authService.login(request.user);
