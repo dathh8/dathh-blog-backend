06f746b0f4fd5336269d1865d34212d5276db171
dathh <dathh@smartosc.com>
2025-06-19 11:49:34 +0700

    config for vercel

--- src/main.ts
+++ src/main.ts
@@ -2,35 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-import { Server } from 'http';
-
-let server: Server;
-
-export default async function handler(req, res) {
-  if (!server) {
-    const app = await NestFactory.create(AppModule);
-    await app.init();
-    const configService = app.get(ConfigService);
-    const port = configService.get('PORT');
-    app.useGlobalPipes(new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true
-    }));
-    app.setGlobalPrefix('api/v1', { exclude: [''] });
-
-    //config cors
-    app.enableCors(
-      {
-        "origin": true,
-        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        "preflightContinue": false,
-        credentials: true
-      }
-    );
-    server = app.getHttpServer();
-  }
-  server.emit('request', req, res);
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -54,4 +25,4 @@ async function bootstrap() {
 
   await app.listen(port);
 }
-bootstrap();
+export default bootstrap();
