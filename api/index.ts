4fb9dcdb4b71b7ba0e7729891bd9fbc363e12996
dathh <dathh@smartosc.com>
2025-06-19 16:06:50 +0700

    try to add cors config

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,28 @@
-import '../src/main';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+
+let cachedServer: any = null;
+
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule, { bodyParser: false });
+    await app.init();
+    cachedServer = app.getHttpAdapter().getInstance();
+  }
+  return cachedServer;
+}
+
+export default async (req: IncomingMessage, res: ServerResponse) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://dathh-blog.vercel.app');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
+  const server = await bootstrapServer();
+  server(req, res);
+};
